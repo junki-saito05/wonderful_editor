1b5fc24b2af0dfe57c66cf27b27ff7030b41bda6
齊藤洵紀 <Junki.ggg@gmail.com>
2023-06-12 22:24:49 +0900

    新規登録・ログイン・ログアウト機能の実装

--- app/controllers/api/v1/base_api_controller.rb
+++ app/controllers/api/v1/base_api_controller.rb
@@ -1,8 +1,12 @@
 class Api::V1::BaseApiController < ApplicationController
-  #before_action :authenticate_user!
+  #include DeviseTokenAuth::Concerns::SetUserByToken
+  alias_method :current_user, :current_api_v1_user
+  alias_method :authenticate_user!, :authenticate_api_v1_user!
+  alias_method :user_signed_in?, :api_v1_user_signed_in?
+  #before_action :authenticate_user!, only: [:create, :update, :destroy]
 
-  def current_user
-    @current_user ||= User.first
-  end
+  # def current_user
+  #   @current_user ||= User.first
+  # end
   # ここに共通処理を実装する
 end
