14f786969e7436755c86a89d23ac61d4892a6bea
齊藤洵紀 <Junki.ggg@gmail.com>
2023-06-14 23:08:30 +0900

    既存のAPIの修正

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -4,7 +4,7 @@
 #
 #  id         :bigint           not null, primary key
 #  body       :text
-#  status     :integer          default(0), not null
+#  status     :integer          default(NULL), not null
 #  title      :string
 #  created_at :datetime         not null
 #  updated_at :datetime         not null
@@ -21,29 +21,58 @@
 require 'rails_helper'
 
 RSpec.describe Article, type: :model do
-  context '記事を新規作成する場合' do
-    it '下書き記事として保存できること' do
-      user = FactoryBot.create(:user)
-      article = Article.create(
-        user: user,
-        title: '下書き記事',
-        body: 'これは下書き記事です。',
-        publication_status: 'draft'
-      )
-      expect(article).to be_valid
-      expect(article.publication_status).to eq 'draft'
+  describe "正常系" do
+    context "タイトルと本文が入力されているとき" do
+      let(:article) { build(:article) }
+
+      it "下書き状態の記事が作成できる" do
+        expect(article).to be_valid
+        expect(article.status).to eq "draft"
+      end
     end
 
-    fit '公開記事として保存できること' do
-      user = FactoryBot.create(:user)
-      article = Article.create(
-        user: user,
-        title: '公開記事',
-        body: 'これは公開記事です。',
-        publication_status: 'published'
-      )
-      expect(article).to be_valid
-      expect(article.publication_status).to eq 'published'
+    context "status が下書き状態のとき" do
+      let(:article) { build(:article, :draft) }
+      it "記事を下書き状態で作成できる" do
+        expect(article).to be_valid
+        expect(article.status).to eq "draft"
+      end
+    end
+
+    context "status が公開状態のとき" do
+      let(:article) { build(:article, :published) }
+      it "記事を公開状態で作成できる" do
+        expect(article).to be_valid
+        expect(article.status).to eq "published"
+      end
     end
   end
 end
+
+# RSpec.describe Article, type: :model do
+#   context '記事を新規作成する場合' do
+#     fit '下書き記事として保存できること' do
+#       user = FactoryBot.create(:user)
+#       article = Article.create(
+#         user: user,
+#         title: '下書き記事',
+#         body: 'これは下書き記事です。'
+#         status: 'draft'
+#       )
+#       expect(article).to be_valid
+#       expect(article.status).to eq "draft"
+#     end
+
+#     it '公開記事として保存できること' do
+#       user = FactoryBot.create(:user)
+#       article = Article.create(
+#         user: user,
+#         title: '公開記事',
+#         body: 'これは公開記事です。',
+#         status: 'published'
+#       )
+#       expect(article).to be_valid
+#       expect(article.status).to eq 'published'
+#     end
+#   end
+# end
