017a234ff4d01747484942f581acf3e6be13d6d1
齊藤洵紀 <Junki.ggg@gmail.com>
2023-06-15 21:15:00 +0900

    フロントエンドとの繋ぎこみ part.3

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,11 @@ Rails.application.routes.draw do
   get "sign_up", to: "home#index"
   get "sign_in", to: "home#index"
   get "articles/new", to: "home#index"
+  get "articles/draft", to: "home#index"
+  get "articles/drafts/:id/edit", to: "home#index"
+  get "articles/:id/edit", to: "home#index"
   get "articles/:id", to: "home#index"
+  get "mypage", to: "home#index"
 
   namespace :api do
     namespace :v1 do
@@ -13,13 +17,11 @@ Rails.application.routes.draw do
         registrations: "api/v1/auth/registrations",
       }
       namespace :articles do
-         resources :drafts, only: [:index, :show]
+        resources :drafts, only: [:index, :show]
       end
-
       namespace :current do
         resources :articles, only: [:index]
       end
-
       resources :articles
     end
   end
